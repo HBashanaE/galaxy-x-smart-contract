b0216f57b240f726494b925358c8450e0f8e1b43
Bashana Elikewela <hbashanae@gmail.com>
2021-12-31 13:28:34 +0530

    add tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,168 @@
+import * as dotenv from "dotenv";
 import { expect } from "chai";
+import { utils } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+dotenv.config();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe("Planet", function () {
+  it("Should deployed", async function () {
+    const Planet = await ethers.getContractFactory("Planet");
+    const planet = await Planet.deploy();
+    let deployed;
+    try {
+      await planet.deployed();
+      deployed = true
+    } catch (error) {
+      deployed = false
+    }
+    expect(deployed).to.equal(true, "Contract does not deployed correctly");
   });
+
+  it("Should flip sale state", async function () {
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    try {
+      await planet.deployed();
+      const flipSateStateTx = await planet.flipSaleState()
+      await flipSateStateTx.wait();
+    } catch (error) {
+    }
+    const saleState = await planet.isSaleActive()
+    expect(saleState).to.equal(true, "Sale state not flipped");
+  });
+
+  it("Should not flip sale state", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    try {
+      await planet.deployed();
+      const flipSateStateTx = await planet.connect(addr1).flipSaleState()
+      await flipSateStateTx.wait();
+    } catch (error) {
+    }
+    const saleState = await planet.isSaleActive()
+    expect(saleState).to.equal(false, "Sale state flipped by not user");
+  });
+
+  it("Should mint new token", async function () {
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    let minted;
+    try {
+      await planet.deployed();
+
+      await planet.flipSaleState()
+
+      await planet.setBaseURI(process.env.BASE_URI)
+
+      const noOfTokens = 1
+      await planet.mint(noOfTokens, { value: utils.parseEther(`${0.002 * noOfTokens}`) })
+      minted = true
+    } catch (error) {
+      minted = false
+    }
+    expect(minted).to.equal(true, "Token does not minted correctly");
+  });
+
+  it("Should not mint new token - sale is not active", async function () {
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    let minted;
+    try {
+      await planet.deployed();
+
+      await planet.setBaseURI(process.env.BASE_URI)
+
+      const noOfTokens = 1
+      await planet.mint(noOfTokens, { value: utils.parseEther(`${0.002 * noOfTokens}`) })
+      minted = true
+    } catch (error) {
+      minted = false
+    }
+    expect(minted).to.equal(false, "Token minted while sale is inactive");
+  });
+
+  it("Should not mint new token - insuficient eth value", async function () {
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    let minted;
+    try {
+      await planet.deployed();
+
+      await planet.flipSaleState()
+
+      await planet.setBaseURI(process.env.BASE_URI)
+
+      const noOfTokens = 1
+      await planet.mint(noOfTokens, { value: utils.parseEther(`${0.001 * noOfTokens}`) })
+      minted = true
+    } catch (error) {
+      minted = false
+    }
+    expect(minted).to.equal(false, "Token minted for low eth value");
+  });
+
+  it("Should not mint new token - exceeds max public mint limit", async function () {
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    let minted;
+    try {
+      await planet.deployed();
+
+      await planet.flipSaleState()
+
+      await planet.setBaseURI(process.env.BASE_URI)
+
+      const noOfTokens = 100
+      await planet.mint(noOfTokens, { value: utils.parseEther(`${0.002 * noOfTokens}`) })
+      minted = true
+    } catch (error) {
+      minted = false
+    }
+    expect(minted).to.equal(false, "More tokens minted than allowed");
+  });
+
+  it("Should not mint new token - exceeds token supply", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+
+    const Planet = await ethers.getContractFactory("Planet");
+
+    const planet = await Planet.deploy();
+    let minted;
+    try {
+      await planet.deployed();
+
+      await planet.flipSaleState()
+
+      await planet.setBaseURI(process.env.BASE_URI)
+  
+      const noOfTokens = 10
+      for(let i = 0; i < 10000; i++){
+        await planet.mint(noOfTokens, { value: utils.parseEther(`${0.002 * noOfTokens}`) })
+      }
+
+      console.log('Minted 100000 tokens')
+
+      await planet.mint(noOfTokens, { value: utils.parseEther(`${0.002 * noOfTokens}`) })
+     
+      minted = true
+    } catch (error) {
+      minted = false
+    }
+    expect(minted).to.equal(false, "More tokens minted than supply");
+  }).timeout(100000);
 });
