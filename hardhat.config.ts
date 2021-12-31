7b5720a7be4fd6d2ba0499d2cddb5d4ad1f25615
Bashana Elikewela <hbashanae@gmail.com>
2021-12-31 12:39:16 +0530

    basic token contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,14 +4,13 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (_taskArgs: any, hre: any) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -30,14 +29,14 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    matic: {
+      url: process.env.MATIC_URL,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gas: 2100000,
+      gasPrice: 8000000000
+    }
+  }
 };
 
 export default config;
